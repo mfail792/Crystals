9a8cc7991d6eaabfc2799dd462083d651871f767
Magus <mfail792@gmail.com>
2019-07-20 01:38:13 -0500

    added jquery key events for adding crystal totals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,28 +19,59 @@ $(document).ready()
 function Start() {
     randNum = numbers[Math.floor(Math.random() * numbers.length)];
     //pushing random chosen number to HTML
-    document.getElementById("random").innerHTML =  " " + randNum.join(" ");
+    document.getElementById("random").innerHTML = " " + randNum.join(" ");
 }
 
-//assigning random values to crystals at start
+//assigning random values 1-10 to crystals at start
 function Values() {
-document.getElementById("red").innerHTML =
-Math.floor(Math.random() * 10);
+    red = document.getElementById("red").innerHTML =
+        Math.floor(Math.random() * 10);
 
-document.getElementById("blue").innerHTML =
-Math.floor(Math.random() * 10);
+    blue = document.getElementById("blue").innerHTML =
+        Math.floor(Math.random() * 10);
 
-document.getElementById("green").innerHTML =
-Math.floor(Math.random() * 10);
+    green = document.getElementById("green").innerHTML =
+        Math.floor(Math.random() * 10);
 
-document.getElementById("orange").innerHTML =
-Math.floor(Math.random() * 10);
+    orange = document.getElementById("orange").innerHTML =
+        Math.floor(Math.random() * 10);
 }
+
+//console logging
+console.log(red);
+console.log(blue);
+console.log(green);
+console.log(orange);
+
 
 //loading the starting functions upon new game
 Start()
 Values()
 
-//add values of crystals when clicked and relay to score box
+//on crystal press, add stored value to total score
+$(red).keypress(function () {
+    document.getElementById("red").innerHTML = " " + score.join(" ");
+    console.log(score);
+});
+
+$(green).keypress(function () {
+    document.getElementById("green").innerHTML = " " + score.join(" ");
+    console.log(score);
+});
+
+$(blue).keypress(function () {
+    document.getElementById("blue").innerHTML = " " + score.join(" ");
+    console.log(score);
+});
+
+$(orange).keypress(function () {
+    document.getElementById("orange").innerHTML = " " + score.join(" ");
+    console.log(score);
+});
+
+//if user matches the randNum variable, then they WIN
+
+
+
 
 
