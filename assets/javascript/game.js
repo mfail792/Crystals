58ad8143861ac745e0641b1798005b73a4cec7db
Magus <mfail792@gmail.com>
2019-07-20 00:22:46 -0500

    added function for random crystal #'s on startup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 //declaring variables
-var randNum = "";
+var randNum = [];
 var numbers = [45, 87, 54, 66, 37, 24, 72];
 var score = 0;
+var number = 0;
+var green = [];
+var red = [];
+var blue = [];
+var orange = [];
 
-//counter variables
-var blueGUess = [];
-var greenGuess = [];
-var redGuess = [];
-var orangeGuess = [];
+//counters
 var wins = 0;
 var losses = 0;
 
@@ -17,6 +18,25 @@ $(document).ready()
 //starts game by drawing random number from numbers array and pushing to HTML
 function Start() {
     randNum = numbers[Math.floor(Math.random() * numbers.length)];
+    //pushing random chosen number to HTML
     document.getElementById("random").innerHTML =  " " + randNum.join(" ");
 }
 
+//assigning random values to crystals at start
+function Values() {
+document.getElementById("red").innerHTML =
+Math.floor(Math.random() * 10);
+
+document.getElementById("blue").innerHTML =
+Math.floor(Math.random() * 10);
+
+document.getElementById("green").innerHTML =
+Math.floor(Math.random() * 10);
+
+document.getElementById("orange").innerHTML =
+Math.floor(Math.random() * 10);
+}
+
+//loading the starting functions upon new game
+Start()
+Values()
