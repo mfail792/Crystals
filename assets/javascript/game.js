07c87ca21e05efa0170e8415992d490eb1ac1a4e
Magus <mfail792@gmail.com>
2019-07-19 23:16:32 -0500

    replaced numbers in array with larger values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //declaring variables
 var randNum = "";
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+var numbers = [45, 87, 54, 66, 37, 24, 72];
 var score = 0;
 
 //counter variables
@@ -11,7 +11,7 @@ var orangeGuess = [];
 var wins = 0;
 var losses = 0;
 
-//Jquery command to start functions upon page load
+//Jquery command to start functions upon DOM load
 $(document).ready()
 
 //starts game by drawing random number from numbers array and pushing to HTML
